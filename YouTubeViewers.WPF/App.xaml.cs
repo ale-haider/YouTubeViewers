d930a6af2ebb7652763997450df2593ae967f785
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-08-07 02:04:08 -0700

    modified app.xamal to change startup

--- YouTubeViewers.WPF/App.xaml.cs
+++ YouTubeViewers.WPF/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using YouTubeViewers.WPF.ViewModel;
 
 namespace YouTubeViewers.WPF
 {
@@ -9,6 +10,17 @@ namespace YouTubeViewers.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new YouTubeViewersViewModel()
+            };
+
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
