243140194105e310cddf2f894db9c5c0c8eb7850
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-08-07 02:09:48 -0700

    added view youtubeViewersView

---
+++ YouTubeViewers.WPF/Views/YouTubeViewersView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace YouTubeViewers.WPF.Views
+{
+ 
+    public partial class YouTubeViewersView : UserControl
+    {
+        public YouTubeViewersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
