c954750a8d6751e2b8db518a771d2f51b96fa08d
= <=>
2026-08-11 03:55:30 -0700

    added a base ViewModel to inherit other models from this base model

---
+++ YouTubeViewers.WPF/ViewModel/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace YouTubeViewers.WPF.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+
+        protected virtual void OnPropertyChanged( string propertyName)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        protected virtual void Dispose() { }
+
+    }
+}
