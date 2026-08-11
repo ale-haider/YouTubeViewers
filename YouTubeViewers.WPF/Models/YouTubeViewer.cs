9efa21a1dacf75bacba59a15e3fbcde353f449e0
= <=>
2026-08-11 03:53:14 -0700

    added models to handel listing left bar and fetch details for details right

---
+++ YouTubeViewers.WPF/Models/YouTubeViewer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeViewers.WPF.Models
+{
+    public class YouTubeViewer
+    {
+        public string Username { get;  }
+        public bool IsSubscribed { get;  }
+        public bool IsMember { get;  }
+
+        public YouTubeViewer(string username, bool isSubscribed, bool isMember)
+        {
+            Username = username;
+            IsSubscribed = isSubscribed;
+            IsMember = isMember;
+        }
+
+    }
+}
