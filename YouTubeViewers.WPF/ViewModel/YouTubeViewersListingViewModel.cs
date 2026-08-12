43fb9aa2d4351b040d3fe90278b181c371c661f6
= <=>
2026-08-12 03:51:51 -0700

    listings of userse list veiw details list viewmodel

---
+++ YouTubeViewers.WPF/ViewModel/YouTubeViewersListingViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using YouTubeViewers.WPF.Models;
+using YouTubeViewers.WPF.Stores;
+
+namespace YouTubeViewers.WPF.ViewModel
+{
+    class YouTubeViewersListingViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<YouTubeViewersListingItemViewModel> _youTubeViewersListingItemViewModels;
+        private readonly SelectedYouTubeViewerStore _selectedYouTubeViewerStore;
+
+        public IEnumerable<YouTubeViewersListingItemViewModel> YouTubeViewersListingItemViewModels => _youTubeViewersListingItemViewModels;
+
+        private YouTubeViewersListingItemViewModel _selectedYouTubeViewerListingItemViewModel;
+        public YouTubeViewersListingItemViewModel SelectedYouTubeViewerListingItemViewModel
+        {
+            get
+            {
+                return _selectedYouTubeViewerListingItemViewModel;
+            }
+            set
+            {
+                _selectedYouTubeViewerListingItemViewModel = value;
+                OnPropertyChanged(nameof(SelectedYouTubeViewerListingItemViewModel));
+
+                _selectedYouTubeViewerStore.SelectedYouTubeViewer = _selectedYouTubeViewerListingItemViewModel?.YouTubeViewer ;
+            }
+        }
+
+        public YouTubeViewersListingViewModel(SelectedYouTubeViewerStore selectedYouTubeViewerStore)
+        {
+             _selectedYouTubeViewerStore = selectedYouTubeViewerStore;
+            
+            _youTubeViewersListingItemViewModels = new ObservableCollection<YouTubeViewersListingItemViewModel>();
+
+            _youTubeViewersListingItemViewModels.Add(new YouTubeViewersListingItemViewModel(new YouTubeViewer("Ali Haider", true, false)));
+            _youTubeViewersListingItemViewModels.Add(new YouTubeViewersListingItemViewModel(new YouTubeViewer("Irtaza", false, false)));
+            _youTubeViewersListingItemViewModels.Add(new YouTubeViewersListingItemViewModel(new YouTubeViewer("Waseem", true, false)));
+
+        }
+
+
+    }
+}
+
+
