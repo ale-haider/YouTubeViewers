0707812bbed6e4bdf248071735934b2719f1f087
= <=>
2026-08-12 03:46:25 -0700

    added stores for viewModels to comunicate with the models

---
+++ YouTubeViewers.WPF/Stores/SelectedYouTubeViewerStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YouTubeViewers.WPF.Models;
+
+namespace YouTubeViewers.WPF.Stores
+{
+    public class SelectedYouTubeViewerStore
+    {
+        private  YouTubeViewer _selectedYouTubeViewer;
+        public YouTubeViewer SelectedYouTubeViewer
+        {
+            get
+            {
+                return _selectedYouTubeViewer;
+            }
+            set
+            {
+                _selectedYouTubeViewer = value;
+                SelectedYouTubeViewerChanged?.Invoke(); 
+            }
+        }
+
+        public event Action SelectedYouTubeViewerChanged;
+
+    }
+}
