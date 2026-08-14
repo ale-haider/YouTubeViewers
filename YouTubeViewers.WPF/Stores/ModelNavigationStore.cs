afd2a3b6f042753e11b4915d57dab1bb30d862f1
= <=>
2026-08-14 03:55:13 -0700

    added a new store to handel model navigaion

---
+++ YouTubeViewers.WPF/Stores/ModelNavigationStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YouTubeViewers.WPF.ViewModel;
+
+namespace YouTubeViewers.WPF.Stores
+{
+    public class ModelNavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+
+            set
+            {
+                _currentViewModel = value;
+
+                CurrentViewModelChanged?.Invoke();
+            }
+        }
+
+
+        public event Action CurrentViewModelChanged;
+        
+    }
+}
