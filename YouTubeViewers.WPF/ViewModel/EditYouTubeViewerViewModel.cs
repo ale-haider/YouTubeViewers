591fab6e11d260cf878ea87c64c178654edb649c
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-08-17 11:01:59 +0500

    added modelViewer view Model

---
+++ YouTubeViewers.WPF/ViewModel/EditYouTubeViewerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeViewers.WPF.ViewModel
+{
+    public class EditYouTubeViewerViewModel
+    {
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public EditYouTubeViewerViewModel()
+        {
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(); 
+        }
+
+    }
+}
